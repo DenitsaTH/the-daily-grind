8812f83d88502927ca426e51fef14eeea9d885d5
DenitsaTH <denizza.cristova@gmail.com>
2024-06-21 22:22:13 +0300

    arrays practice - two sum - 2nd ver

--- 01. arrays/two_sum.py
+++ 01. arrays/two_sum.py
@@ -18,3 +18,41 @@ class Solution:
                 return [i, vals[res]]
             
             vals[nums[i]] = i
+
+
+# Version two: sort the array and use binary search for O(log n) time complexity
+# Downside is that this solution also requires additional memory to keep the original pre-sorting indices
+
+class Solution:
+    def twoSum(self, nums, target):
+
+        def binary_search(start, stop, x):
+
+            while start <= stop:
+
+                mid = start + (stop - start) // 2
+
+                if nums_with_indices[mid][0] == x:
+                    return nums_with_indices[mid][1]
+
+                elif nums_with_indices[mid][0] < x:
+                    start = mid + 1
+
+                else:
+                    stop = mid - 1
+
+            return
+
+        nums_with_indices = [(num, i) for i, num in enumerate(nums)]
+        nums_with_indices.sort()
+
+        for i in range(len(nums_with_indices)):
+            res = target - nums_with_indices[i][0]
+
+            found = binary_search(i+1, len(nums_with_indices) - 1, res)
+
+            if found is not None:
+                return [nums_with_indices[i][1], found]
+    
+        return []
+        
