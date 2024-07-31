830329e156fee7d434a78ef534326b11ac163e10
DenitsaTH <denizza.cristova@gmail.com>
2024-07-31 11:01:40 +0300

    practice: number of 1 bits

---
+++ 08. misc/number_of_1_bits.py
@@ -0,0 +1,23 @@
+'''Write a function that takes the binary representation of a positive integer and returns the number of set bits it has (also known as the Hamming weight).
+
+Follow up: If this function is called many times, how would you optimize it?
+'''
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        cache = {n: res}
+        m = n
+
+        while m > 0:
+            res += m % 2
+            m = m // 2
+
+            if m in cache:
+                res += cache[m]
+
+                return res
+
+        cache[n] = res  # caching the values for optimization
+        return res
